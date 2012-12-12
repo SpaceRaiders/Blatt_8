192de17e31ae7b3e89e5cd36efff27c23f363904
Yorick <yorick@informatik.uni-bremen.de>
2012-12-12 15:52:29 +0100

    Zweidimensionalen Array eingefuehrt fuer transparente Pixel

--- TmpCollision.java
+++ TmpCollision.java
@@ -14,6 +14,96 @@ public class TmpCollision extends Actor
     /**
      * @return Collidierendes Object bzw null 
      */
+    
+    
+    public Actor  getCollidingObject(Class objClass)
+    {
+       List<Actor> actors = getIntersectingObjects(objClass);
+        
+       for( Actor actor : actors )
+       {
+           TmpCollision tmp = (TmpCollision) actor ;
+           if(tmp.isColliding(objClass)){ // hier muss noch allgemein hin
+               return actor;
+            }
+        }
+       return null;
+    }
+    
+    /**
+     * Testet auf Kollision mit einer best KLasse
+     */
+    public boolean isColliding(Class objClass)
+    {
+        //return getOneIntersectingObject(objClass) != null ;
+        
+        
+        //transparenz des einen bildes
+        boolean[][] imgTransparency = getTransPx(getImage());
+        
+        List<Actor> actors = getIntersectingObjects(objClass);
+        
+        for( Actor object : actors){
+            TmpCollision tmp = (TmpCollision) object;
+            boolean[][] tmpTransparency = tmp.getTransPx(getImage());
+            
+            for(int i=0; i< imgTransparency.length ; i++){
+                for( int j=0; j < imgTransparency[0].length; j++){
+                    
+                    
+                }
+            }
+        }
+        
+        
+        
+        
+        
+        
+        return false;
+    }
+    
+    /**
+     * Testet auf neue Kollision( mit einer best. Klasse)
+     * TODO: guter name 
+     */
+    public boolean neueKollsionmit(Class objClass)
+    {
+        List<Actor> objects = getIntersectingObjects(objClass);
+        for(Actor object : objects )
+        {
+            for( Scrollable scrble : collObjcs)
+            {
+                if( object == (Actor) scrble){
+                    collObjcs.add((Scrollable) object );
+                return true;
+                }
+                
+            }
+        }
+        return false;
+    }
+    /**
+     * gibt einen 2 dimensionalen arraz mit den transparenten px zurueck ( true, wenn nicht transparent )
+     */
+    private boolean[][] getTransPx(GreenfootImage img)
+    {
+        boolean[][] imgTransp= new boolean[img.getWidth()][img.getHeight()];
+        
+        for(int i=0;i<img.getWidth();i++){
+            for(int j=0; j < img.getHeight();j++){
+                imgTransp[i][j]= img.getTransparency()==0 ? false : true; // false wenn transparent
+            }
+        }
+        return imgTransp;
+    }
+    
+    
+    //hier der Code vom Anfang
+    
+    
+    
+    /*
     public Actor  getCollidingObject(Class ObjClass)
     {
        Actor actor = getOneIntersectingObject(ObjClass);
@@ -26,10 +116,10 @@ public class TmpCollision extends Actor
             return null;
         }
     }
-    
-    /**
+    */
+    /*
      * Testet auf Kollision mit einer best KLasse
-     */
+     *
     public boolean isColliding(Class objClass)
     {
         return getOneIntersectingObject(objClass) != null ;
@@ -38,7 +128,7 @@ public class TmpCollision extends Actor
     /**
      * Testet auf neue Kollision( mit einer best. Klasse)
      * TODO: guter name 
-     */
+     *//*
     public boolean neueKollsionmit(Class objClass)
     {
         List<Actor> objects = getIntersectingObjects(objClass);
@@ -59,5 +149,5 @@ public class TmpCollision extends Actor
             }
         }
         return false;
-    }
+    }*/
 }
